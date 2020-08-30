47b429134bdf1dbe5feb5bcd39e98a0d2b59aa99
Ronnie Kaczynski <ronniie.k@pm.me>
2020-08-30 16:11:34 +0100

    add glm

---
+++ premake5.lua
@@ -0,0 +1,28 @@
+workspace "cod zombies"
+	configurations {"Debug", "Release"}
+
+project "cod zombies"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	targetdir "bin/%{cfg.buildcfg}"
+	objdir "bin-int/%{cfg.buildcfg}"
+
+	files 
+	{
+		"src/**.h",
+		"src/**.cpp",
+	}
+
+	includedirs
+	{
+		"vendor/glm"
+	}
+
+	filter "configurations:Debug"
+		defines {""}
+		symbols "on"
+
+	filter "configurations:Release"
+		defines {""}
+		optimize "on"
